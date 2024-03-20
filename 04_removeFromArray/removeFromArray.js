77afb0806a39b924b2514e72c53da231a85a5e89
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2024-03-20 01:14:19 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = [];
+  let result = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    result = 1;
+    for (let j = 0; j < args.length; j++) {
+      if (array[i] === args[j]) {
+        result = 0;
+      }
+    }
+    if (result == 1) {
+      newArray.push(array[i]);
+    }    
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
